7c75616b521f225863ecceb7a5a823a2526d50d0
SpaceTech1800 <oliverspace1800@hotmail.com>
2024-12-01 00:03:04 -0800

    'updates'

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -1,28 +1,44 @@
-pragma solidity 0.8;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract Identity {
+contract Identity is MerkleTree {
     struct User {
         string name;
         address walletAddress;
-        bytes32 biometricData;  //store hashhash of biometric data  
+        bytes32 biometricData;  // Hashed biometric data
     }
 
-    mapping (address => User) public users;
+    mapping(address => User) public users;
 
-    function registerUser() {
-        public 
-        returns (bool sucess) {
-            require(_name != "", "Name cannot be empty ");
+    event UserRegistered(address indexed walletAddress, string name);
+    event UserAccessed(address indexed walletAddress, string action);
 
-            if (users[_walletAddress].exist()) {
-                return false //user already exists 
-            }
+    function registeredUser(
+        string memory _name,
+        bytes32 _hashedBiometricData
+    ) external returns (bool) {
+        require(bytes(_name).length > 0, "Name cannot be empty");
+        require(users[msg.sender].walletAddress == address(0), "User already have a wallet address");
 
-            User storage user = user[_walletAddress];
-            user.name = _name;
-            user.walletAddredd = _walletAddress;
+        //register user
+        users[msg.sender] = User({
+            name: _name,
+            walletAddress: msg.sender,
+            biometricData: _hashedBiometricData
+        });
 
-        }
+        //Add hashed biometric data to MerkleTree
+        addEntry(_hahshedBiometricData);
 
+        emit UserRegistered(msg.sender, _name);
+        return true;
+    }
+
+    function redUserBiometric(addresss _walletAddress) external view returns (bytes32) {
+        User memory user = users[_walletAddress];
+        require(user.walletAddress != address(0), "user does not exist");
+
+        emit UserAccessed(_walletAddress, "Read");
+        return user.biometricData;
     }
 }
